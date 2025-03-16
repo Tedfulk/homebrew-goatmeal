562d49e2c893c6659bf2b521b41f1454e68df1f8
Ted Fulk <teddyfulk@gmail.com>
2025-03-16 07:16:16 -0400

    fix

--- Formula/goatmeal.rb
+++ Formula/goatmeal.rb
@@ -1,17 +1,17 @@
 class Goatmeal < Formula
   desc "Goatmeal is a powerful terminal-based application that provides access to various AI chat providers and web search capabilities, all within your terminal."
   homepage "https://github.com/tedfulk/goatmeal"
-  version "1.3.0"
+  version "1.3.1"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.0/goatmeal-darwin-arm64.tar.gz"
+    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.1/goatmeal-darwin-arm64.tar.gz"
     sha256 "cc7737658493eb5c96c1425c015fac5a8ef4960745249c7cef98f5fbc807087a"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.0/goatmeal-darwin-amd64.tar.gz"
+    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.1/goatmeal-darwin-amd64.tar.gz"
     sha256 "84f59b7359d1c162fb53f267804247a4f735cbc7704d66f29ac91a3d857f1bed"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.0/goatmeal-linux-amd64.tar.gz"
-    sha256 "c310cce9425491ae45ad6b3a27dc240724ac7a038c8161a99e9c07c30e53218b"
+    url "https://github.com/tedfulk/goatmeal/releases/download/v1.3.1/goatmeal-linux-amd64.tar.gz"
+    sha256 "0b007b0595d2908df59929bced21abc3f029eacd0e3964a5bce68352b76461e7"
   end
 
   def install
